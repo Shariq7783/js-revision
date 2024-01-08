ef5df622f836800b2264774e4805e3d7bd0bdc68
Shariq Bhat <156027213+Shariq7783@users.noreply.github.com>
2024-01-08 23:09:25 +0000

    Variables and Datatypes in javascript

---
+++ Basics/var&dt.js
@@ -0,0 +1,13 @@
+console.log('Variables and Datatypes in JavaScript');
+/*
+    number
+    bigint
+    boolean
+    string
+    null
+    undefined
+*/
+ 
+console.log(typeof 232);
+console.log(typeof null);
+console.log(typeof undefined)
